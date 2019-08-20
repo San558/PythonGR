3d5b2641e4618a7cc9f401f7edacb4275b1dec5d
Santhosh <40908966+San558@users.noreply.github.com>
2019-08-20 19:45:04 +0530

    GUI using Python

---
+++ Python GUI/window.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 13:53:30 2019
+
+@author: Santhosh
+"""
+
+import tkinter as tk
+root = tk.Tk()
+label = tk.Label(root, text="Hello World", padx=10, pady=10) 
+label.pack() 
+root.mainloop()
